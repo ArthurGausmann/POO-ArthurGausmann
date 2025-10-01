312d26e7a0f6912b69f7934efea7e5623f5f3ede
ArthurGausmann <arthur.campellogausmann@gmail.com>
2025-09-30 21:25:01 -0300

    Exercício 2 - TODO: Métodos

---
+++ Pratica02/Exercicio2/src/exercicio2/Elevador.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio2;
+
+/**
+ *
+ * @author arthurg5
+ */
+public class Elevador {
+    private int totalAndares;
+    private int andarAtual;
+    private int capacidadeTotal;
+    private int pessoas;
+    
+    Elevador(int tA, int aA, int cT, int p) {
+        totalAndares = tA;
+        andarAtual = aA;
+        capacidadeTotal = cT;
+        pessoas = p;
+    }
+}

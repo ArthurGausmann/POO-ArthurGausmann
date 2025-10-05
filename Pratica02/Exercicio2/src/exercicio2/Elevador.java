c058a3ea1413ba8555de679da0c985405f8bc55a
ArthurGausmann <arthur.campellogausmann@gmail.com>
2025-10-05 14:06:52 -0300

    Pratica 02 Exercício 2 Concluído

--- Pratica02/Exercicio2/src/exercicio2/Elevador.java
+++ Pratica02/Exercicio2/src/exercicio2/Elevador.java
@@ -9,15 +9,86 @@ package exercicio2;
  * @author arthurg5
  */
 public class Elevador {
-    private int totalAndares;
+    // Atributos
     private int andarAtual;
+    private int totalAndares;
     private int capacidadeTotal;
     private int pessoas;
     
-    Elevador(int tA, int aA, int cT, int p) {
-        totalAndares = tA;
-        andarAtual = aA;
-        capacidadeTotal = cT;
-        pessoas = p;
+    // Construtor
+    Elevador(int andarAtual, int totalAndares, int capacidadeTotal, int pessoas) {
+        this.andarAtual = andarAtual;
+        this.totalAndares = totalAndares;
+        this.capacidadeTotal = capacidadeTotal;
+        this.pessoas = pessoas;
+    }
+    
+    Elevador(int totalAndares, int capacidadeTotal) {
+        this(0, totalAndares, capacidadeTotal, 0);
+    }
+    
+    // Métodos
+    void inicia(int totalAndares, int capacidadeTotal) {
+        this.totalAndares = totalAndares;
+        this.capacidadeTotal = capacidadeTotal;
+        
+        
+        System.out.println("Elevador inicializado com os seguintes parâmetros: ");
+        System.out.println("Andar atual: " + andarAtual);
+        System.out.println("Total de andares: " + totalAndares);
+        System.out.println("Capacidade total: " + capacidadeTotal);
+        System.out.println("Pessoas: " + pessoas + "\n");
+    }
+    
+    void entra() {
+        if (capacidadeTotal == pessoas + 1) {
+            System.out.println("Capacidade total atingida\n");
+        } else {
+            pessoas++;
+            System.out.println(pessoas + "/" + capacidadeTotal + "\n");
+        }
+    }
+    
+    void sai() {
+        if (pessoas < 1) {
+            System.out.println("Não há ninguém no elevador\n");
+        } else {
+            pessoas--;
+            System.out.println(pessoas + "/" + capacidadeTotal + "\n");
+        }
+    }
+    
+    void sobe() {
+        if (andarAtual == totalAndares) {
+            System.out.println("O elevador está no último andar\n");
+        } else {
+            andarAtual++;
+            System.out.println(andarAtual + "/" + totalAndares + "\n");
+        }
+    }
+    
+    void desce() {
+        if (andarAtual < 0) {
+            System.out.println("O elevador está no térreo\n");
+        } else {
+            andarAtual--;
+            System.out.println(andarAtual + "/" + totalAndares + "\n");
+        }
+    }
+    
+    void andar(int andar) {
+        if (andar > totalAndares) {
+            System.out.println("Este andar não tem no prédio");
+        } else {
+            andarAtual = andar;
+            System.out.println("Andar atual: " + andarAtual + "\n");
+        }
+    }
+    
+    void info() {
+        System.out.println("Andar atual: " + andarAtual);
+        System.out.println("Total de andares: " + totalAndares);
+        System.out.println("Capacidade total: " + capacidadeTotal);
+        System.out.println("Pessoas: " + pessoas + "\n");
     }
 }
--- Pratica02/Exercicio2/src/exercicio2/Exercicio2.java
+++ Pratica02/Exercicio2/src/exercicio2/Exercicio2.java
@@ -14,7 +14,34 @@ public class Exercicio2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Elevador elevador1 = new Elevador(26, 13);
+        
+        elevador1.inicia(26, 13);
+        
+        elevador1.entra();
+        elevador1.entra();
+        elevador1.entra();
+        elevador1.entra();
+        
+        elevador1.sobe();
+        elevador1.sobe();
+        elevador1.sobe();
+        elevador1.sobe();
+        elevador1.sobe();
+        
+        elevador1.sai();
+        
+        elevador1.andar(15);
+        
+        elevador1.sai();
+        elevador1.sai();
+        
+        elevador1.sobe();
+        
+        elevador1.sai();
+        elevador1.sai();
+        
+        elevador1.info();
     }
     
 }
